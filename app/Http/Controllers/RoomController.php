569067dcc883b500adbd3b075d7626f3e1edf78a
mikehiphip <pipat.pimnont@gmail.com>
2025-07-01 15:50:34 +0700

    UPDATE

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -105,7 +105,13 @@ class RoomController extends Controller
 
     public function reserve_form_check_user(Request $request){
         $keyword = $request->input('keyword');
-        $room = Room::where('name', $keyword)->first();
+        // $room = Room::where('name', $keyword)->first();
+        $room = Room::orderBy('rooms.ref_floor_id','ASC')
+            ->whereHas('floor.building', function ($query) {
+                $query->where('ref_branch_id', session("branch_id"));
+            })
+            ->where('rooms.name',$keyword)
+            ->first();
         return response()->json([
             'found' => $room ? true : false
         ]);
