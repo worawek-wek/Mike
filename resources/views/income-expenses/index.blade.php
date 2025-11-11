9d6c87060c12938396fdc027bca80ab15c8757a3
mikehiphip <pipat.pimnont@gmail.com>
2025-11-11 14:29:35 +0700

    UPDATE

--- resources/views/income-expenses/index.blade.php
+++ resources/views/income-expenses/index.blade.php
@@ -193,11 +193,10 @@
                                             <div class="d-flex align-items-center mb-2 mb-md-0">
                                                 <label class="">Show</label>
                                                 <select onchange='loadData("{{$page_url}}/datatable")' name="limit" class="form-select ms-2 me-2 p_search" style="width:100px">
-                                                    <option value="10">10</option>
                                                     <option value="25">25</option>
                                                     <option value="50">50</option>
-                                                    <option value="75">75</option>
                                                     <option value="100">100</option>
+                                                    <option value="200">200</option>
                                                 </select>
                                             </div>
                                         </div>
